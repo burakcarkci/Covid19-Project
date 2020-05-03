245e6cda8e1f8b702ae5708f4f477786cbde847b
Burak <burakcarkci@hotmail.com>
2020-05-03 16:53:29 -0400

    Deleted unnecessary/unused fields from Statistic Response class

--- Covid19/app/src/main/java/com/example/covid_19/StatsResponse.java
+++ Covid19/app/src/main/java/com/example/covid_19/StatsResponse.java
@@ -8,8 +8,6 @@ import java.util.Date;
 public class StatsResponse {
     @SerializedName("Global")
     public Global global;
-    @SerializedName("Countries")
-    public ArrayList<Countries> countries = new ArrayList<Countries>();
 }
 
 class Global{
@@ -45,55 +43,3 @@ class Global{
     }
 }
 
-class Countries {
-    public String Country;
-    public String CountryCode;
-    public String Slug;
-    public int NewConfirmed;
-    public int TotalConfirmed;
-    public int NewDeaths;
-    public int TotalDeaths;
-    public int NewRecovered;
-    public int TotalRecovered;
-    public Date Date;
-
-    public String getCountry() {
-        return Country;
-    }
-
-    public String getCountryCode() {
-        return CountryCode;
-    }
-
-    public String getSlug() {
-        return Slug;
-    }
-
-    public int getNewConfirmed() {
-        return NewConfirmed;
-    }
-
-    public int getTotalConfirmed() {
-        return TotalConfirmed;
-    }
-
-    public int getNewDeaths() {
-        return NewDeaths;
-    }
-
-    public int getTotalDeaths() {
-        return TotalDeaths;
-    }
-
-    public int getNewRecovered() {
-        return NewRecovered;
-    }
-
-    public int getTotalRecovered() {
-        return TotalRecovered;
-    }
-
-    public java.util.Date getDate() {
-        return Date;
-    }
-}
